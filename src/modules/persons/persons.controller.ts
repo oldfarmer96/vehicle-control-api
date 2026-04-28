1c95747732bc20746c766bfd5862b3635e463191
oldfarmer96 <oldfarmer996@gmail.com>
2026-04-28 00:51:40 -0500

    refactor: person controller

--- src/modules/persons/persons.controller.ts
+++ src/modules/persons/persons.controller.ts
@@ -1,11 +1,8 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Get,
-  HttpStatus,
   Param,
-  ParseUUIDPipe,
   Patch,
   Post,
   Query,
@@ -15,46 +12,39 @@ import { CreatePersonDto } from './dto/create-person.dto';
 import { FindPersonsQryDto } from './dto/find-persons-qry.dto';
 import { UpdateAccessStatusDto } from './dto/update-access-status.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { Auth } from '@/common/decorators/auth.decorator';
+import { RolWeb } from '@/generated/prisma/enums';
+import { UUIDPipe } from '@/common/pipes/parse-uuid.pipe';
 
 @Controller('persons')
 export class PersonsController {
   constructor(private readonly personService: PersonsService) {}
 
   @Post()
+  @Auth(RolWeb.ADMINISTRADOR)
   createPerson(@Body() dto: CreatePersonDto) {
     return this.personService.createPerson(dto);
   }
 
   @Get()
+  @Auth(RolWeb.ADMINISTRADOR)
   getAllPersons(@Query() qry: FindPersonsQryDto) {
     return this.personService.getAllPersons(qry);
   }
 
   @Patch(':id/access-status')
+  @Auth(RolWeb.ADMINISTRADOR)
   toggleAccessStatus(
-    @Param(
-      'id',
-      new ParseUUIDPipe({
-        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-        exceptionFactory: () => new BadRequestException('invalid id'),
-      }),
-    )
-    id: string,
+    @Param('id', UUIDPipe) id: string,
     @Body() dto: UpdateAccessStatusDto,
   ) {
     return this.personService.updateAccessStatus(id, dto.status);
   }
 
   @Patch(':id')
+  @Auth(RolWeb.ADMINISTRADOR)
   updatePerson(
-    @Param(
-      'id',
-      new ParseUUIDPipe({
-        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-        exceptionFactory: () => new BadRequestException('invalid id'),
-      }),
-    )
-    id: string,
+    @Param('id', UUIDPipe) id: string,
     @Body() dto: UpdatePersonDto,
   ) {
     return this.personService.updatePerson(id, dto);
