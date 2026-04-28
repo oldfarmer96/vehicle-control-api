1878885fe9d513ced8b9ab71162725ebae43ed02
oldfarmer96 <oldfarmer996@gmail.com>
2026-04-28 02:12:23 -0500

    refactor: websockets

--- src/modules/access-events/access-events.gateway.ts
+++ src/modules/access-events/access-events.gateway.ts
@@ -6,39 +6,39 @@ import {
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { PayloadI } from './interfaces/payloadEvent.interface';
 
 @WebSocketGateway({
-  cors: { origin: '*' }, // Ajustar según tu dominio del frontend
+  cors: { origin: '*' },
   namespace: 'access-control',
 })
 export class AccessEventsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  private readonly logger = new Logger('AccessEventsGateway');
+  private readonly logger = new Logger(AccessEventsGateway.name);
+
   @WebSocketServer()
   server!: Server;
 
-  // 1. Se ejecuta cuando el servidor de WS se inicia
-  afterInit(server: Server) {
+  afterInit() {
     this.logger.log(
-      '🚀 Servidor WebSocket inicializado y escuchando en el namespace: /access-control',
+      'Servidor WebSocket inicializado y escuchando en el namespace: /access-control',
     );
   }
 
-  // 2. Se ejecuta cuando un cliente (tu dashboard) se conecta
   handleConnection(client: Socket) {
-    this.logger.log(`✅ Cliente conectado: ${client.id}`);
+    this.logger.log(`Cliente conectado: ${client.id}`);
   }
 
-  // 3. Se ejecuta cuando un cliente se desconecta
   handleDisconnect(client: Socket) {
-    this.logger.log(`❌ Cliente desconectado: ${client.id}`);
+    this.logger.log(`Cliente desconectado: ${client.id}`);
   }
 
-  notifyNewEvent(payload: any) {
+  notifyNewEvent(payload: PayloadI) {
     this.logger.log(
-      `📡 Emitiendo nuevo evento de acceso: ${payload.vehiculo.placa}`,
+      `Emitiendo nuevo evento de acceso: ${payload.vehiculo.placa}`,
     );
+
     this.server.emit('new-access-event', payload);
   }
 }
